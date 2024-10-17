f8987cc460d8b0e571917ab3ec1ce0845bc09e14
Ksenia-rgb <ksenialymova@mail.ru>
2024-10-17 14:42:22 +0300

    funcs for reading and writing matrix

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include "matx.h"
+
+void input(int** t, size_t M, size_t N)
+{
+  for (size_t i = 0; i < M; i++)
+  {
+    for (size_t j = 0; i < N; j++)
+    {
+      std::cin >> t[i][j];
+    }
+  }
+}
