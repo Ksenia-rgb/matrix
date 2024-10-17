132b57743d7c452c65535bbaa76121f8f465f39f
Ksenia-rgb <ksenialymova@mail.ru>
2024-10-17 11:16:55 +0300

    funcs for matrix

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+int** create(size_t M, size_t N);
+void destroy(int** table, size_t M, size_t N);
+
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
+  int** table = create(M, N);
+  destroy(table, M, N);
   std::cout << M << " " << N << "\n";
 }
