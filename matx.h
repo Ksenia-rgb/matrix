66afdf5bf5119291c1d888e2c614477d911a690a
Ksenia-rgb <ksenialymova@mail.ru>
2024-10-17 11:40:08 +0300

    changind head

---
+++ matx.h
@@ -0,0 +1,9 @@
+#ifndef MTX_H
+#define MTX_H
+#include <cstddef>
+int** create(size_t M, size_t N);
+void destroy(int** table, size_t M, size_t N);
+void input (int** table, size_t M, size_t N);
+void output(const int * const * table, size_t M, size_t N);
+
+#endif
