7bf07598f73b8d4d90373369e6c6dedd5de8cb4a
Ksenia-rgb <ksenialymova@mail.ru>
2024-10-17 11:29:18 +0300

    head file

--- mtx.h
+++ mtx.h
@@ -1,12 +1,13 @@
 #include <iostream>
-#include "mtx.h"
+
+int** create(size_t M, size_t N);
+void destroy(int** table, size_t M, size_t N);
 
 int main()
 {
   size_t M = 0, N = 0;
   std::cin >> M >> N;
   int** table = create(M, N);
-  input(table, M, N);
-  output(table, M, N);
   destroy(table, M, N);
+  std::cout << M << " " << N << "\n";
 }
